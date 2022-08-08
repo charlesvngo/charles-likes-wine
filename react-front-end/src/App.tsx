3aee11e0eaa210588506378d379a718873eb6dc3
Charles Ngo <charlesngo37@gmail.com>
2022-08-08 09:36:28 -0700

    inital commit. Express and react setup w/ typescript

---
+++ react-front-end/src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hi, I'm Charles and I like drinking wine</h1>
+      <h2>
+        I made this site as a quick reference to list available types of wines,
+        recommended pairings, and to list wines I like.
+      </h2>
+    </div>
+  );
+}
+
+export default App;
