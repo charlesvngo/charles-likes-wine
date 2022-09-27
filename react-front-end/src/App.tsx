33ea87d6805fe58130a0e98eea457b78bbec6563
Charles Ngo <charlesngo37@gmail.com>
2022-09-27 14:37:42 -0700

    setup basic structure of the front page

--- react-front-end/src/App.tsx
+++ react-front-end/src/App.tsx
@@ -3,11 +3,20 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>Hi, I'm Charles and I like drinking wine</h1>
-      <h2>
-        I made this site as a quick reference to list available types of wines,
-        recommended pairings, and to list wines I like.
-      </h2>
+      <h1>App Bar + Drawers</h1>
+      <h1>Rotating Carousel</h1>
+      <h1>Quick links + buttons</h1>
+      <h1>Inside Scoop section showcasing products</h1>
+      <h1>Card to show you can pickup instore</h1>
+      <h1>Take the wine quiz</h1>
+      <h1>Curated wine section</h1>
+      <h1>Top rated wine section</h1>
+      <h1>Explore the top rated vintages</h1>
+      <h1>Find Us banner for map</h1>
+      <h1>
+        Lists for About, Help, Contact, and sign up to newsletter + contact
+      </h1>
+      <h1>Bottom banner for copywrite and privacy policy</h1>
     </div>
   );
 }
