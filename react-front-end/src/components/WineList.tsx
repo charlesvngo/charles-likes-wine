118f5bb96a2d426237916ca97e89a90e53be09b6
Charles Ngo <charlesngo37@gmail.com>
2022-10-04 21:05:29 -0700

    remove template lines

--- react-front-end/src/components/WineList.tsx
+++ react-front-end/src/components/WineList.tsx
@@ -1,9 +1,7 @@
 import ListSubheader from "@mui/material/ListSubheader";
 import List from "@mui/material/List";
 import ListItemButton from "@mui/material/ListItemButton";
-import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
-import SendIcon from "@mui/icons-material/Send";
 
 const WineList = () => {
   const redWines = [
